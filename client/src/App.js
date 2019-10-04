552154badbf842ee812d777487eaa4e4617a8d97
Larry Glebov <larry.glebov@gmail.com>
2019-10-04 15:21:41 +0200

    added _Home_, _About_ pages, configured router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 import './App.css'
 
-const App = () => <div className="App">
-  my app
-</div>
+const App = () => <Router>
+  <Fragment>
+    <Navbar />
+    <div className="container">
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <About exact path='/about' component={About} />
+      </Switch>
+    </div>
+  </Fragment>
+</Router>
 
 export default App
